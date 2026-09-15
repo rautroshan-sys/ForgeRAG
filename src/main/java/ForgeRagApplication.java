078c54be133f403831241fbb80485f570ccc7044
Roshan <rautroshan0206@gmail.com>
2026-09-15 17:32:53 +0530

    Add Changes

---
+++ src/main/java/ForgeRagApplication.java
@@ -0,0 +1,12 @@
+package com.forgerag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForgeRagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForgeRagApplication.class, args);
+    }
+}
